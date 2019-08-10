b25723959e350e4287282c9de24f372dddb393a9
lukastatarunas <lukasttatarunas@gmail.com>
2019-08-10 22:05:45 +0300

    Updates

---
+++ precache-manifest.3d975fa44e2c55871fc013d711185815.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends-Web-App/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "10845352a7e409fa629d",
+    "url": "/Robofriends-Web-App/static/js/runtime~main.10845352.js"
+  },
+  {
+    "revision": "bebfeb482444cbf15eb7",
+    "url": "/Robofriends-Web-App/static/js/main.bebfeb48.chunk.js"
+  },
+  {
+    "revision": "5c2e0afaa7276865705f",
+    "url": "/Robofriends-Web-App/static/js/1.5c2e0afa.chunk.js"
+  },
+  {
+    "revision": "bebfeb482444cbf15eb7",
+    "url": "/Robofriends-Web-App/static/css/main.81fa6d16.chunk.css"
+  },
+  {
+    "revision": "5c2e0afaa7276865705f",
+    "url": "/Robofriends-Web-App/static/css/1.76609d60.chunk.css"
+  },
+  {
+    "revision": "88e46cbcc9fd9cb2c30ebf16c78ab1bf",
+    "url": "/Robofriends-Web-App/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Robofriends-Web-App/precache-manifest.d9fc686f863b6a45654254af80bc1ad5.js"
+  "/Robofriends-Web-App/precache-manifest.3d975fa44e2c55871fc013d711185815.js"
 );
 
 workbox.clientsClaim();
